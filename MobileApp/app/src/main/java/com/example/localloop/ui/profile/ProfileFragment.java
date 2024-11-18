8bd0e42fdd4be3f510782232d9aade120b36df87
Marcelo Filipe Marques <2200428@my.ipleiria.com>
2024-11-18 23:08:05 +0000

    Ajustar fragmento de Dashboard para Profile

--- MobileApp/app/src/main/java/com/example/localloop/ui/profile/ProfileFragment.java
+++ MobileApp/app/src/main/java/com/example/localloop/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.example.localloop.ui.dashboard;
+package com.example.localloop.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,20 +12,20 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.localloop.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
