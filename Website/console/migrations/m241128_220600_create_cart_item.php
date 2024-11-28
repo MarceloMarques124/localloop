518c248c3fcd8189aeb7c2a96906b87bc2d7a0b3
Marcelo Filipe Marques <2200428@my.ipleiria.com>
2024-11-28 22:15:47 +0000

    Migração para criar a tabela cart_item

---
+++ Website/console/migrations/m241128_220600_create_cart_item.php
@@ -0,0 +1,69 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241128_220600_create_cart_item
+ */
+class m241128_220600_create_cart_item extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('cart_item', [
+            'card_id' => $this->integer()->notNull(),
+            'trade_proposal_id' => $this->integer()->notNull(),
+            'created_at' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP')->append('ON UPDATE CURRENT_TIMESTAMP'),
+        ], 'ENGINE=InnoDB');
+
+        $this->addPrimaryKey(
+            'pk_cart_item',
+            'cart_item',
+            ['card_id', 'trade_proposal_id']
+        );
+
+        $this->addForeignKey(
+            'fk_cart_item_cart',
+            'cart_item',
+            'card_id',
+            'card',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk_cart_item_trade_proposal',
+            'cart_item',
+            'trade_proposal_id',
+            'trade_proposal',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('cart_item');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m241128_220600_create_cart_item cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
