9486042553681b7dacde008afc36319cb562d8a2
Marcelo Filipe Marques <2200428@my.ipleiria.com>
2024-12-14 17:54:08 +0000

    Criar Base model que vai ter as datas de criacao/update

---
+++ MobileApp/app/src/main/java/com/example/localloop/models/BaseModel.java
@@ -0,0 +1,24 @@
+package com.example.localloop.models;
+
+import java.time.LocalDateTime;
+
+public class BaseModel {
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
