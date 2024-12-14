dac9034752217237d7f98ced93e844b86276ddb1
Marcelo Filipe Marques <2200428@my.ipleiria.com>
2024-12-14 17:54:20 +0000

    Criar classe Trade

---
+++ MobileApp/app/src/main/java/com/example/localloop/models/Trade.java
@@ -0,0 +1,40 @@
+package com.example.localloop.models;
+
+public class Trade extends BaseModel {
+    private int id;
+    private int advertisementId;
+    private int userInfoId;
+    private int state;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAdvertisementId() {
+        return advertisementId;
+    }
+
+    public void setAdvertisementId(int advertisementId) {
+        this.advertisementId = advertisementId;
+    }
+
+    public int getUserInfoId() {
+        return userInfoId;
+    }
+
+    public void setUserInfoId(int userInfoId) {
+        this.userInfoId = userInfoId;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
