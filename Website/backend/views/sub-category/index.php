e3445a7f1d245437a419fa701fcd9116942653b5
joao <2220892@my.ipleiria.pt>
2024-12-31 11:03:25 +0000

    category e sub category

---
+++ Website/backend/views/sub-category/index.php
@@ -0,0 +1,47 @@
+<?php
+
+use common\models\SubCategory;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var backend\models\SubCategorySearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Sub Categories';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="sub-category-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Sub Category', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'category_id',
+            'created_at',
+            'updated_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, SubCategory $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
