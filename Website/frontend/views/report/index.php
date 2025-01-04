56eb9d73df26b7d826d719d52262c97e0b6f3147
joao <2220892@my.ipleiria.pt>
2025-01-04 21:21:05 +0000

    crud generator

---
+++ Website/frontend/views/report/index.php
@@ -0,0 +1,49 @@
+<?php
+
+use common\models\Report;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var frontend\models\ReportSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Reports';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="report-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Report', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'author_id',
+            'user_info_id',
+            'trade_id',
+            'advertisement_id',
+            //'created_at',
+            //'updated_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Report $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
