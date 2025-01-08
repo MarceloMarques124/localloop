4a2b34ba9cb4e075e9300fa4dc19f97238122eb6
Marcelo Filipe Marques <2200428@my.ipleiria.pt>
2025-01-08 21:54:31 +0000

    limpar sidebar

--- Website/backend/views/layouts/sidebar.php
+++ Website/backend/views/layouts/sidebar.php
@@ -4,24 +4,8 @@ use hail812\adminlte\widgets\Menu;
 
 ?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
-
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="<?= $assetDir ?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
 
         <!-- SidebarSearch Form -->
         <!-- href be escaped -->
@@ -46,8 +30,6 @@ use hail812\adminlte\widgets\Menu;
 
                         ['label' => 'Yii2 PROVIDED', 'header' => true],
                         ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                        ['label' => 'Gii', 'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
-                        ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
 
                         ['label' => 'Users', 'url' => ['user-info/index']],
                         ['label' => 'Advertisements', 'url' => ['advertisement/index']],
