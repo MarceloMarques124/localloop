9466b59bab1c11d714d35ece24839bc5e44fcee7
Marcelo Filipe Marques <2200428@my.ipleiria.pt>
2025-01-19 13:08:49 +0000

    Adicionar layout de fazer proposta a um anúncio

--- MobileApp/app/src/main/java/com/localloop/ui/proposal/MakeProposalViewModel.java
+++ MobileApp/app/src/main/java/com/localloop/ui/proposal/MakeProposalViewModel.java
@@ -2,7 +2,13 @@ package com.localloop.ui.proposal;
 
 import com.localloop.ui.BaseViewModel;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
 public class MakeProposalViewModel extends BaseViewModel {
+    @Inject
     public MakeProposalViewModel() {
         super();
     }
