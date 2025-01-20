8d09f9f46a73b7b49ef4dd555f744c8255e28bc7
Marcelo Filipe Marques <2200428@my.ipleiria.pt>
2025-01-20 01:57:02 +0000

    Colocar endpoint de signup

--- Website/frontend/modules/api/controllers/AuthController.php
+++ Website/frontend/modules/api/controllers/AuthController.php
@@ -5,8 +5,11 @@ namespace frontend\modules\api\controllers;
 use common\models\User;
 use common\models\UserInfo;
 use Yii;
+use yii\db\Exception;
 use yii\rest\ActiveController;
+use yii\validators\EmailValidator;
 use yii\web\BadRequestHttpException;
+use yii\web\ServerErrorHttpException;
 use yii\web\UnauthorizedHttpException;
 
 class AuthController extends ActiveController
@@ -34,4 +37,47 @@ class AuthController extends ActiveController
 
         return $user->getAuthKey();
     }
+
+    /**
+     * @throws Exception
+     * @throws BadRequestHttpException
+     * @throws ServerErrorHttpException
+     */
+    public function actionSignup()
+    {
+        $request = Yii::$app->request;
+        $username = $request->post('username');
+        $password = $request->post('password');
+        $email = $request->post('email');
+
+        if (empty($username) || empty($password) || empty($email)) {
+            throw new BadRequestHttpException('Username, password, and email are required.');
+        }
+
+        $emailValidator = new EmailValidator();
+        if (!$emailValidator->validate($email)) {
+            throw new BadRequestHttpException('The email format is invalid.');
+        }
+
+        if (User::findByUsername($username)) {
+            throw new BadRequestHttpException('Username is already taken.');
+        }
+
+        if (User::findOne(['email' => $email])) {
+            throw new BadRequestHttpException('Email is already registered.');
+        }
+
+        $user = new User();
+        $user->username = $username;
+        $user->email = $email;
+        $user->setPassword($password);
+        $user->generateAuthKey();
+
+        if (!$user->save()) {
+            throw new ServerErrorHttpException('Failed to create the user for unknown reasons.');
+        }
+        
+        return $user->getAuthKey();
+    }
+
 }
