19f5033ab423310f2a64f910f10ef1ea0cae3fd4
Marcelo Filipe Marques <2200428@my.ipleiria.pt>
2025-01-28 22:55:52 +0000

    Atualizar sdk

--- MobileApp/app/build.gradle.kts
+++ MobileApp/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.localloop"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.localloop"
         minSdk = 34
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
