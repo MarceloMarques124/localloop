81858055c92af20a14986b86afe7be2e298d9dcd
Marcelo Filipe Marques <2200428@my.ipleiria.pt>
2025-01-29 20:26:21 +0000

    Remover alterações do build graddle

--- MobileApp/app/build.gradle.kts
+++ MobileApp/app/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.security.crypto)
     implementation(libs.material)
-    implementation(libs.legacy.support.v4)
     annotationProcessor(libs.hilt.compiler)
 
     testImplementation(libs.junit)
