862762a0fec6439dde0626f01f833f9d68eea400
Marcelo Marques <2200428@ipleiria.pt>
2025-02-06 22:03:48 +0000

    Base para demonstrar como fazer

--- MobileApp/app/src/main/java/com/localloop/ui/profile/ProfileFragment.java
+++ MobileApp/app/src/main/java/com/localloop/ui/profile/ProfileFragment.java
@@ -4,32 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.localloop.databinding.FragmentDashboardBinding;
+import com.localloop.data.models.User;
+import com.localloop.databinding.FragmentProfileBinding;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class ProfileFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private ProfileViewModel profileViewModel;
+    private FragmentProfileBinding binding;
 
+    private User currentUser;
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel profileViewModel =
+        profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
 
-        final TextView textView = binding.textDashboard;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        profileViewModel.getCurrentUser();
+
+        profileViewModel.getUserLiveData().observe(getViewLifecycleOwner(), user -> binding.userEmail.setText(user.getEmail()));
+
+        return binding.getRoot();
     }
 
     @Override
--- MobileApp/app/src/main/java/com/localloop/ui/profile/ProfileViewModel.java
+++ MobileApp/app/src/main/java/com/localloop/ui/profile/ProfileViewModel.java
@@ -2,24 +2,42 @@ package com.localloop.ui.profile;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
+
+import com.localloop.api.repositories.CurrentUserRepository;
+import com.localloop.data.models.User;
+import com.localloop.ui.BaseViewModel;
+import com.localloop.utils.DataCallBack;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
 
 @HiltViewModel
-public class ProfileViewModel extends ViewModel {
+public class ProfileViewModel extends BaseViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final CurrentUserRepository currentUserRepository;
+    private final MutableLiveData<User> userLiveData = new MutableLiveData<>();
 
     @Inject
-    public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+    public ProfileViewModel(CurrentUserRepository currentUserRepository) {
+        this.currentUserRepository = currentUserRepository;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<User> getUserLiveData() {
+        return userLiveData;
+    }
+
+    public void getCurrentUser() {
+        currentUserRepository.getUser(new DataCallBack<>() {
+            @Override
+            public void onSuccess(User user) {
+                userLiveData.setValue(user);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                error.setValue(errorMessage);
+            }
+        });
     }
 }
